091613d001502154c8270ff30f940525c79c4d1b
Sivamuthu Kumar <sivamuthukumar07@gmail.com>
2020-08-15 00:30:26 -0400

    chnages

--- coffee-service/src/database.providers.ts
+++ coffee-service/src/database.providers.ts
@@ -1,9 +1,8 @@
 import * as mongoose from 'mongoose';
-
 export const databaseProviders = [
     {
         provide: 'DATABASE_CONNECTION',
         useFactory: (): Promise<typeof mongoose> =>
-            mongoose.connect(`mongodb://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@mongodb.default.svc.cluster.local/coffee`, { useNewUrlParser: true }),
+            mongoose.connect(`mongodb://mongodb.default.svc.cluster.local/coffee`, { useNewUrlParser: true }),
     },
 ];
