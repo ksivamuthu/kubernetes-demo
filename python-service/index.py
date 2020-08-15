4fb7182290c55b7fa9b859f31b27037ce4207f37
Sivamuthu Kumar <sivamuthukumar07@gmail.com>
2020-08-15 01:27:49 -0400

    Fix python code

--- python-service/index.py
+++ python-service/index.py
@@ -1,9 +1,9 @@
-From flask import Flask
+from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world:
+def hello_world():
     return 'I\'m python service'
 
-if __name == '__main__':
+if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=3001)
