bc2568ac2392f1b8b39035151380d5b60f020c3f
Sivamuthu Kumar <sivamuthukumar07@gmail.com>
2020-08-15 00:19:11 -0400

    chnages

--- coffee-service/src/database.providers.ts
+++ coffee-service/src/database.providers.ts
@@ -4,6 +4,6 @@ export const databaseProviders = [
     {
         provide: 'DATABASE_CONNECTION',
         useFactory: (): Promise<typeof mongoose> =>
-            mongoose.connect(`mongodb://admin:Dnrs41Bq60@mongodb.default.svc.cluster.local/coffee`, { useNewUrlParser: true }),
+            mongoose.connect(`mongodb://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@mongodb.default.svc.cluster.local/coffee`, { useNewUrlParser: true }),
     },
 ];
